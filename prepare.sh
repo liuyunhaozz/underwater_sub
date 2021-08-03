161170e257482f92038e4f5b2336564c6a43a09d
ubuntu <ubuntu@localhost.localdomain>
2021-08-04 01:54:08 +0800

    modified prepare.sh again

--- prepare.sh
+++ prepare.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-cd ~/underwater_sub/data
-unzip -d . test-B-image.zip 
-cd ..
+cd ~/underwater_sub
+unzip -d . data.zip 
 mkdir results
 mkdir submit
 
+cd data
+unzip -d . test-B-image.zip
+cd ..
+
 conda create -n my_mmcv python=3.7 -y
 conda activate my_mmcv
-conda install cython
-conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.1 -c pytorch
+conda install cython -y
+conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.1 -c pytorch -y
 
-wget https://download.openmmlab.com/mmcv/dist/1.1.5/torch1.6.0/cu101/mmcv_full-1.1.5%2Btorch1.6.0%2Bcu101-cp37-cp37m-manylinux1_x86_64.whl
-pip install https://download.openmmlab.com/mmcv/dist/1.1.5/torch1.6.0/cu101/mmcv_full-1.1.5%2Btorch1.6.0%2Bcu101-cp37-cp37m-manylinux1_x86_64.whl
+pip install mmcv_full-1.1.5+torch1.6.0+cu101-cp37-cp37m-manylinux1_x86_64.whl
 pip install albumentations
 pip install tqdm
 pip install -r requirements/build.txt
