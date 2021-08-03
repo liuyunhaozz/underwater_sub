59415d9082efde0888f768f7cb934fcaa545a197
ubuntu <ubuntu@localhost.localdomain>
2021-08-04 01:43:07 +0800

    modified two sh files

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-conda activate mmcv
-cd ~/waterdetection
+conda activate my_mmcv
+cd ~/underwater_sub
 chmod +x tools/dist_test.sh
-./tools/dist_test.sh configs/final.py work_dirs/final/epoch_24.pth 8 --format-only --eval-options "jsonfile_prefix=./results/testB"
+./tools/dist_test.sh configs/final.py data/final_0803.pth 1 --format-only --eval-options "jsonfile_prefix=./results/testB"
 python tools/process_data/json2submit.py
