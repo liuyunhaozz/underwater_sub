013429f1f83179d3bd3f2fee59c36b7bdfa4a00a
ubuntu <ubuntu@localhost.localdomain>
2021-08-04 01:56:03 +0800

    fix bugs in prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -14,7 +14,7 @@ conda activate my_mmcv
 conda install cython -y
 conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.1 -c pytorch -y
 
-pip install mmcv_full-1.1.5+torch1.6.0+cu101-cp37-cp37m-manylinux1_x86_64.whl
+pip install data/mmcv_full-1.1.5+torch1.6.0+cu101-cp37-cp37m-manylinux1_x86_64.whl
 pip install albumentations
 pip install tqdm
 pip install -r requirements/build.txt
