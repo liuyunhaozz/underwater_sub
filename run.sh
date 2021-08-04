ebbcb9d3575d0f2f04070e2c90262a482a293cb2
ubuntu <ubuntu@localhost.localdomain>
2021-08-04 18:29:13 +0800

    fix bugs mkdir results

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 source activate
 conda activate my_mmcv
 cd /userhome/underwater_sub
+mkdir results
 python ./tools/process_data/generate_testB_json.py
 python ./tools/main_test.py configs/final.py final_0803.pth 1 --format-only --eval-options "jsonfile_prefix=./results/testB"
 python tools/process_data/json2submit.py
